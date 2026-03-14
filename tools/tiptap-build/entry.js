df89373cebdf26f708fe5c0d79dc6b8531856fe0
Erik Hagen <erikhag1git@gmail.com>
2026-03-14 13:07:20 +0100

    Legg til selvhostet TipTap-bundle og byggesett

---
+++ tools/tiptap-build/entry.js
@@ -0,0 +1,9 @@
+export { Editor } from '@tiptap/core';
+export { default as StarterKit } from '@tiptap/starter-kit';
+export { Table } from '@tiptap/extension-table';
+export { TableRow } from '@tiptap/extension-table-row';
+export { TableCell } from '@tiptap/extension-table-cell';
+export { TableHeader } from '@tiptap/extension-table-header';
+export { Link } from '@tiptap/extension-link';
+export { Image } from '@tiptap/extension-image';
+export { Markdown } from 'tiptap-markdown';
